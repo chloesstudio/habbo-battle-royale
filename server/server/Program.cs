ec5d194fc33d624d1e53ea7153adecfaabb43ece
Noel <41082110+hamiltondev@users.noreply.github.com>
2018-09-21 19:18:24 +0200

    a commit message ig

---
+++ server/server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Habbo {
+  internal class Program {
+    private static void Main(string[] args) {
+      Console.Title = "Habbo Battle Royale Server";
+
+      Console.WriteLine("  _    _       _     _             ____        _   _   _        _____                   _      " + Environment.NewLine +
+              " | |  | |     | |   | |           |  _ \\      | | | | | |      |  __ \\                 | |     " + Environment.NewLine +
+              " | |__| | __ _| |__ | |__   ___   | |_) | __ _| |_| |_| | ___  | |__) |___  _   _  __ _| | ___ " + Environment.NewLine +
+              " |  __  |/ _` | '_ \\| '_ \\ / _ \\  |  _ < / _` | __| __| |/ _ \\ |  _  // _ \\| | | |/ _` | |/ _ \\" + Environment.NewLine +
+              " | |  | | (_| | |_) | |_) | (_) | | |_) | (_| | |_| |_| |  __/ | | \\ \\ (_) | |_| | (_| | |  __/" + Environment.NewLine +
+              " |_|  |_|\\__,_|_.__/|_.__/ \\___/  |____/ \\__,_|\\__|\\__|_|\\___| |_|  \\_\\___/ \\__, |\\__,_|_|\\___|" + Environment.NewLine +
+              "                                                                             __/ |             " + Environment.NewLine +
+              "                                                                            |___/              " + Environment.NewLine);
+      try {
+        new WebSocket("127.0.0.1", 7777);
+      }
+      catch(Exception exception) {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+
+        Console.WriteLine(exception.Message);
+
+        Console.ResetColor();
+      }
+      
+
+      while(true)
+        Console.ReadLine();
+    }
+  }
+}
---
+++ server/server/Source/WebSocket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WatsonWebsocket;
+
+namespace Habbo {
+  internal class WebSocket {
+    private readonly WatsonWsServer Socket;
+
+    public WebSocket(string address, int port) {
+      Socket = new WatsonWsServer(address, port, false, true, null, OnClientConnect, OnClientDisconnect, OnMessageReceived, false);
+    }
+
+    private bool OnMessageReceived(string ipPort, byte[] data) {
+      throw new NotImplementedException();
+    }
+
+    private bool OnClientDisconnect(string ipPort) {
+      throw new NotImplementedException();
+    }
+
+    private bool OnClientConnect(string ipPort, IDictionary<string, string> data) {
+      throw new NotImplementedException();
+    }
+  }
+}
